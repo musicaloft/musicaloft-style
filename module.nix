b9842777d73da9a5dd1b763b083f84e4df332d13
municorn <municorn@musicaloft.com>
2025-09-15 16:35:56 -0600

    feat: configure devenv shell

--- module.nix
+++ module.nix
@@ -8,8 +8,69 @@ localFlake:
   ];
 
   perSystem =
-    { ... }:
+    { config, pkgs, ... }:
     {
+      devenv.shells.default = {
+        packages = [ config.treefmt.build.wrapper ];
+
+        # git hooks
+        git-hooks.hooks = {
+          # commit linting
+          commitlint-rs =
+            let
+              config = pkgs.writers.writeYAML "commitlintrc.yml" {
+                rules = {
+                  description-empty.level = "error";
+                  description-format = {
+                    level = "error";
+                    format = "^[a-z].*$";
+                  };
+                  description-max-length = {
+                    level = "error";
+                    length = 72;
+                  };
+                  scope-max-length = {
+                    level = "warning";
+                    length = 10;
+                  };
+                  scope-empty.level = "warning";
+                  type = {
+                    level = "error";
+                    options = [
+                      "build"
+                      "chore"
+                      "ci"
+                      "docs"
+                      "feat"
+                      "fix"
+                      "perf"
+                      "refactor"
+                      "style"
+                      "test"
+                    ];
+                  };
+                };
+              };
+
+            in
+            {
+              enable = true;
+              name = "commitlint-rs";
+              package = pkgs.commitlint-rs;
+              description = "Validate commit messages with commitlint-rs";
+              entry = "${pkgs.lib.getExe pkgs.commitlint-rs} -g ${config} -e";
+              always_run = true;
+              stages = [ "commit-msg" ];
+            };
+
+          # format on commit
+          treefmt = {
+            enable = true;
+            packageOverrides.treefmt = config.treefmt.build.wrapper;
+          };
+        };
+      };
+
       treefmt.programs = {
         rustfmt.enable = true;
         nixfmt.enable = true;

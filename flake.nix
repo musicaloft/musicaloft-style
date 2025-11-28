e2190e9d89ed56aa5a9ae306bc15d384cf07c647
municorn <municorn@musicaloft.com>
2025-11-27 18:54:21 -0700

    refactor: restructure flake to export reusable devenv modules

--- flake.nix
+++ flake.nix
@@ -2,12 +2,31 @@
   description = "The standardized Musicaloft code style";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
+    devenv = {
+      url = "github:cachix/devenv";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    git-hooks-nix = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    devenv-root = {
+      url = ./.devenv/root;
+      flake = false;
     };
   };
 
@@ -21,21 +40,31 @@
       in
       {
         imports = [
-          inputs.flake-parts.flakeModules.flakeModules
+          inputs.devenv.flakeModule
+          inputs.git-hooks-nix.flakeModule
+          inputs.treefmt-nix.flakeModule
+
+          # dogfood: use our own devenv modules
           flakeModule
         ];
+
         systems = [
           "x86_64-linux"
           "aarch64-linux"
           "aarch64-darwin"
           "x86_64-darwin"
         ];
-        perSystem =
-          { config, ... }:
-          {
-            devShells.default = config.treefmt.build.devShell;
+
+        flake = {
+          inherit flakeModule;
+
+          # export devenv modules for other projects to use
+          devenvModules = {
+            default = ./devenv;
+            commitlint = ./devenv/commitlint.nix;
+            treefmt = ./devenv/treefmt.nix;
           };
-        flake.flakeModule = flakeModule;
+        };
       }
     );
 }

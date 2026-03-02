0348c801383541ee60929b5b2afe8306c241db8b
municorn <municorn@musicaloft.com>
2026-03-02 10:16:24 -0700

    fix(treefmt): remove dprint and malva in favor of oxfmt

--- devenv/style/treefmt.nix
+++ devenv/style/treefmt.nix
@@ -1,32 +1,7 @@
-{ pkgs, ... }:
 {
   treefmt = {
     enable = true;
     config.programs = {
-      dprint = {
-        enable = true;
-        includes = [ "*.scss" ];
-        settings = {
-          indentWidth = 2;
-          useTabs = false;
-
-          plugins = pkgs.dprint-plugins.getPluginList (
-            plugins: with plugins; [
-              g-plane-malva
-            ]
-          );
-
-          malva = {
-            hexColorLength = "short";
-            quotes = "preferSingle";
-            formatComments = true;
-            declarationOrder = "smacss";
-            keyframeSelectorNotation = "keyword";
-            preferSingleLine = true;
-          };
-        };
-      };
-
       kdlfmt.enable = true;
       nixfmt.enable = true;
       oxfmt.enable = true;

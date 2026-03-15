68ac89ed8759b09793c0bff79dbab3e1b12df199
municorn <municorn@musicaloft.com>
2026-03-15 09:34:31 -0600

    feat: use files to configure commitlintrc

--- modules/files.nix
+++ modules/files.nix
@@ -4,5 +4,41 @@
       printWidth = 100;
       proseWrap = "always";
     };
+
+    ".commitlintrc".yaml = {
+      rules = {
+        description-empty.level = "error";
+        description-format = {
+          level = "error";
+          format = "^[a-z].*$";
+        };
+        description-max-length = {
+          level = "error";
+          length = 72;
+        };
+        scope-max-length = {
+          level = "warning";
+          length = 10;
+        };
+        scope-empty.level = "warning";
+        type = {
+          level = "error";
+          options = [
+            "build"
+            "chore"
+            "ci"
+            "docs"
+            "dx"
+            "feat"
+            "fix"
+            "perf"
+            "refactor"
+            "revert"
+            "style"
+            "test"
+          ];
+        };
+      };
+    };
   };
 }
--- modules/hooks.nix
+++ modules/hooks.nix
@@ -4,43 +4,6 @@
   pkgs,
   ...
 }:
-let
-  commitlintConfig = pkgs.writers.writeYAML "commitlintrc.yml" {
-    rules = {
-      description-empty.level = "error";
-      description-format = {
-        level = "error";
-        format = "^[a-z].*$";
-      };
-      description-max-length = {
-        level = "error";
-        length = 72;
-      };
-      scope-max-length = {
-        level = "warning";
-        length = 10;
-      };
-      scope-empty.level = "warning";
-      type = {
-        level = "error";
-        options = [
-          "build"
-          "chore"
-          "ci"
-          "docs"
-          "dx"
-          "feat"
-          "fix"
-          "perf"
-          "refactor"
-          "revert"
-          "style"
-          "test"
-        ];
-      };
-    };
-  };
-in
 {
   git-hooks.hooks = {
     # clippy for rust projects
@@ -83,7 +46,7 @@ in
       name = "commitlint-rs";
       package = pkgs.commitlint-rs;
       description = "Validate commit messages with commitlint-rs";
-      entry = "${pkgs.lib.getExe pkgs.commitlint-rs} -g ${commitlintConfig} -e";
+      entry = "${pkgs.lib.getExe pkgs.commitlint-rs} -e";
       pass_filenames = true;
       stages = [ "commit-msg" ];
     };

2d519452c7555d67f4e4276fb967890c3f82571f
municorn <municorn@musicaloft.com>
2026-03-26 14:52:54 -0600

    refactor(rust): remove explicit clippy hook and toolchain variable
    musicaloft-shell takes care of this automatically now

--- templates/rust/devenv.nix
+++ templates/rust/devenv.nix
@@ -9,19 +9,10 @@ let
   buildInputs = [ ];
   nativeBuildInputs = with pkgs; [ autoPatchelfHook ];
   libraryPath = lib.makeLibraryPath buildInputs;
-  toolchain = config.languages.rust.toolchainPackage;
 in
 {
   # needed for dynamic linking at runtime
   env.RUSTFLAGS = lib.mkForce "-C link-args=-Wl,-fuse-ld=mold,-rpath,${libraryPath}";
-
-  git-hooks.hooks.clippy = {
-    enable = true;
-    packageOverrides = {
-      cargo = toolchain;
-      clippy = toolchain;
-    };
-  };
 
   languages.rust = {
     enable = true;
